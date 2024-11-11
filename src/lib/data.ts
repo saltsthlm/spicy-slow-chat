8aca10fe722996b9699a6fec7f265601e924b266
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-11 16:33:34 +0100

    following drizzle setup
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,7 @@
+const db = drizzle({ schema });
+
 export async function fetchMessages() {
-  const data = await sql`SELECT * FROM messages`;
+  const data = await db.select().from(messages);
 
   return data;
 }
