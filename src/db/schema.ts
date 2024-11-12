be80b78e47668cc8ef2165ecaccb35714ad31d1c
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-12 13:14:13 +0100

    Improvement: move db set-up to repository
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const messagesTable = pgTable("messages", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  userId: integer().notNull(),
-  timeStamp: integer().notNull(),
-  content: varchar().notNull(),
-});
--- src/lib/data.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/db";
-import { messagesTable } from "@/db/schema";
-
-
-export async function FetchMessages() {
-  const data = await db.select().from(messagesTable);
-  return data;
-}
-
