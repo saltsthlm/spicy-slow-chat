1b925d95f7c36fff8c6d1a0e7cae5021bee5ce89
Odin Thif <mjd.thif@icloud.com>
2024-11-12 13:36:54 +0100

    set up message chat feature
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/chat/actions.ts
+++ src/features/chat/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { createFeature } from "./feature";
+const chatFeature = createFeature();
 
 export async function postMessageAction(formData: FormData) {
   const message = formData.get("message");
