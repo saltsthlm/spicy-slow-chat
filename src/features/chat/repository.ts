2812e93733fe38fcd6d1907046cf6f51d278e94c
Odin Thif <mjd.thif@icloud.com>
2024-11-12 13:22:20 +0100

    removed old repository
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/chat/repository.ts
+++ src/features/chat/repository.ts
@@ -1,8 +1,24 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const db = drizzle(process.env.DATABASE_URL!);
+
+export const messagesTable = pgTable("messages", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer().notNull(),
+  timeStamp: integer().notNull(),
+  content: varchar().notNull(),
+});
+
+async function FetchMessages() {
+  const data = await db.select().from(messagesTable);
+  return data;
+}
+
 export function createRepository() {
-  // eslint-disable-next-line prefer-const
-  let messages: Message[] = [];
   async function getMessages() {
-    return messages;
+    return await FetchMessages();
   }
   return {
     getMessages,
