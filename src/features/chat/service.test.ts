0ee7aa4fb3d43cedfbb26a9cdf718f70054753d1
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-13 13:18:41 +0100

    call function inside test case, missing the real functiion
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/chat/service.test.ts
+++ src/features/chat/service.test.ts
@@ -7,8 +7,13 @@ describe("Cool down:", () => {
     const messages = ([]) => [];
     deepEqual(messages([]), []);
   });
+ 
+  it("should return empty array when there are no messages | 0 case scenario", () => {
+    const messages = await getAllMessages(latestFetchDate, messages, coolDownTimeInSeconds);
+    deepEqual(messages([]), []);
+  });
 
-  it("should return 1 message on cooldown | 1 case scenario", () => {
+  it("should return 1 message on cool down | 1 case scenario", () => {
     const messages = ([]) => [];
     deepEqual(messages([]), []);
   });
