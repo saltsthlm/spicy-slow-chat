df6dbe82444426cde8e5d722a30119aca866a501
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-13 14:21:47 +0100

    First 0-case scenario is set-up
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/chat/service.test.ts
+++ src/features/chat/service.test.ts
@@ -7,26 +7,22 @@ function calculateCoolDown(latestFetchDate: bigint, messageTimestamp: bigint) {
 }
 
 async function getAllMessages(latestFetchDate: bigint, messages: Message[]) {
-  messages.map((message) =>
+ return messages.map((message) =>
     calculateCoolDown(latestFetchDate, message.timestamp)
       ? message
-      : { ...message, content: "Message is on cool down" }
+      : { ...message, content: "Message is on cool down" },
   );
 }
 
 describe("Cool down:", () => {
   it("Works!", () => {});
-  it("should return empty array when there are no messages | 0 case scenario", () => {
-    const messages = ([]) => [];
-    deepEqual(messages([]), []);
+
+  it("should return empty array when there are no messages | 0 case scenario", async () => {
+    const filteredMessages = await getAllMessages(BigInt(Date.now()), []);
+    deepEqual(filteredMessages, []);
   });
 
-  it("should return empty array when there are no messages | 0 case scenario", () => {
-    const messages = await getAllMessages(latestFetchDate, messages);
-    deepEqual(messages, []);
-  });
-
-  it("should return 1 message on cool down | 1 case scenario", () => {
+  it.skip("should return 1 message on cool down | 1 case scenario", () => {
     const messages = ([]) => [];
     deepEqual(messages([]), []);
   });
