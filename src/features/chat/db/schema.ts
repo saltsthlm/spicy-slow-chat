751e2e89a2bb0ed9141e2bd38a4373c66ac88178
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-13 10:39:41 +0100

    Create fetches table to store fetch history
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/chat/db/schema.ts
+++ src/features/chat/db/schema.ts
@@ -1,9 +1,14 @@
-
 import { integer, pgTable, varchar, bigint } from "drizzle-orm/pg-core";
 
 export const messagesTable = pgTable("messages", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   userId: integer().notNull(),
-  timeStamp: bigint({mode: "bigint"}).notNull(),
+  timeStamp: bigint({ mode: "bigint" }).notNull(),
   content: varchar().notNull(),
+});
+
+export const fetchTable = pgTable("fetches", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer().notNull(),
+  timeStamp: bigint({ mode: "bigint" }).notNull(),
 });
