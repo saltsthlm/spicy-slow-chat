025105a963b181a8141929ad336d5be11597f658
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-13 13:32:10 +0100

    Implement getAllMessages functions for filter out cooldown messages
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/chat/service.test.ts
+++ src/features/chat/service.test.ts
@@ -1,5 +1,16 @@
 import { deepEqual } from "node:assert/strict";
 import { describe, it } from "node:test";
+import { Message } from "./types";
+
+async function getAllMessages(
+  latestFetchDate: bigint,
+  messages: Message[],
+  // coolDownTimeInMillis: bigint,
+) {
+  messages.filter((message) => {
+    return message.timestamp > latestFetchDate + BigInt(3600 * 1000);
+  });
+}
 
 describe("Cool down:", () => {
   it("Works!", () => {});
@@ -7,9 +18,9 @@ describe("Cool down:", () => {
     const messages = ([]) => [];
     deepEqual(messages([]), []);
   });
- 
+
   it("should return empty array when there are no messages | 0 case scenario", () => {
-    const messages = await getAllMessages(latestFetchDate, messages, coolDownTimeInSeconds);
+    // const messages = await getAllMessages(latestFetchDate, messages, coolDownTimeInSeconds);
     deepEqual(messages([]), []);
   });
 
