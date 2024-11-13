ef38538ac325f75c14f9c002c9bde4debde6f6a9
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-13 11:35:27 +0100

    Create zero case for filtered messages
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/chat/service.test.ts
+++ src/features/chat/service.test.ts
@@ -1,5 +1,10 @@
+import { deepEqual } from "node:assert/strict";
 import { describe, it } from "node:test";
 
-describe("Chat Service", () => {
+describe("Cool down:", () => {
   it("Works!", () => {});
+  it("should return empty array when there are no messages | 0 case scenario", () => {
+    const messages = ([]) => [];
+    deepEqual(messages([]), []);
+  });
 });
