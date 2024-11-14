4e53327e89932431cabc8c49cd2877357e09bdd5
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-14 16:17:51 +0100

    Seeding database with faker
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -1,12 +1,18 @@
 import { faker } from "@faker-js/faker";
 export function seedMessages(numberOfMessages: number) {
-  Array.from({ length: numberOfMessages }).map(() => {
-    return faker.word.words(20);
-  }).forEach((message => {
-    console.log(message)
-  }))
-
+  Array.from({ length: numberOfMessages })
+    .map(() => {
+      const content = `${faker.word.noun} ${faker.word.verb} ${faker.word.adverb}`;
+      return {
+        username: faker.person.firstName(),
+        content,
+        timestamp:
+          BigInt(Date.now()) - BigInt(Math.floor(Math.random() * 500000)),
+      };
+    })
+    .forEach((message) => {
+      console.log(message);
+    });
 }
 
-
-seedMessages(15)
+seedMessages(15);
