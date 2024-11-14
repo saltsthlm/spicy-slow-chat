93eecfbf7ff7f53855dc8e6fdc6ea282031d9c25
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-14 14:09:12 +0100

    Add fetch before getting all messages
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/chat/repository.ts
+++ src/features/chat/repository.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import { Message } from "./types";
-import { messagesTable, db } from "./db";
+import { messagesTable, db, fetchTable } from "./db";
 
 export function createRepository() {
   return {
     async getAllMessages() {
+      await db.insert(fetchTable).values({
+        username: "John Wick",
+        timestamp: BigInt(Date.now())
+      })
       return await db.select().from(messagesTable);
     },
 
