647f94f9d0761a22f3e7b786efd3587e0cf9669a
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-14 15:08:57 +0100

    Create User Profile component and add it to the root page
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,19 @@ import {
   Card,
   CardTitle,
 } from "@/features";
+import { UserProfile } from "@/features/user/user-profile";
 
 export default async function Home() {
   const messages = (await chatFeature.service.getAllMessages()).map(
     (message) => {
       return message.content;
-    }
+    },
   );
 
   console.log(messages);
   return (
     <div>
+      <UserProfile />
       <Card className="text-center flex flex-col items-center m-auto mt-4 w-96">
         <CardTitle>Spicy Slow Chat</CardTitle>
         <ChatFeed messages={messages} />
