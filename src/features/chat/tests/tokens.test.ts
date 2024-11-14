71fd5155d6975818edae60d0c48100ee1c414b70
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-14 19:10:36 +0100

    Format code

--- src/features/chat/tests/tokens.test.ts
+++ src/features/chat/tests/tokens.test.ts
@@ -3,34 +3,23 @@ import { describe, it } from "node:test";
 
 describe("Tokens", () => {
   it("should return zero tokens when user don't have tokens | 0 case scenario", async () => {
-    const tokens = {
-      weekly: 0,
-      daily: 0,
-    };
+    const tokens = { weekly: 0, daily: 0 };
     const fetches = 0;
     const result = calculateTokens(fetches, tokens);
 
-    deepEqual(result, {
-      weekly: 0,
-      daily: 0,
-    });
+    deepEqual(result, { weekly: 0, daily: 0 });
   });
 
   it("should return right amount of tokens | 1 case scenario", () => {
-    const tokens = {
-      weekly: 2,
-      daily: 1,
-    };
+    const tokens = { weekly: 2, daily: 1 };
     const fetches = 1;
     const result = calculateTokens(fetches, tokens);
-    deepEqual(result, {
-      weekly: 2,
-      daily: 0,
-    });
+    deepEqual(result, { weekly: 2, daily: 0 });
   });
 });
 
 export type Token = { weekly: number; daily: number };
+
 function calculateTokens(fetches: number, tokens: Token): Token {
   if (fetches === 0) {
     return {
