f907200efe665e46a94efbbf0ba0d1656956204e
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-14 22:36:04 +0100

    Fetch number of tokens for current user and display it in the Chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ import {
 import { UserProfile } from "@/features/user/ui/user-profile";
 
 export default async function Home() {
-  const messages = await chatFeature.service.getAllMessages()
+  const messages = await chatFeature.service.getAllMessages();
+  const numberOfTokens = await chatFeature.service.getUserTokens("John Wick");
 
   return (
     <div className="h-screen max-w-screen-sm m-auto flex flex-col items-center">
@@ -20,6 +21,7 @@ export default async function Home() {
       </h1>
 
       <Card className="flex flex-col items-center m-auto px-6 w-full max-h-full overflow-y-auto flex-grow">
+        <CardHeader>You have {numberOfTokens} fetch tokens.</CardHeader>
         <CardTitle className="w-full">
           <h2 className="scroll-m-20 border-b text-md font-normal tracking-normal first:mt-0 pt-4 pb-3 w-full">
             Spicy Members
