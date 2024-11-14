4582cc53949b3d16a9687a81e2aba2d29bca0403
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-14 14:41:52 +0100

    create test for zero case on fetches table
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/chat/service.test.ts
+++ src/features/chat/service.test.ts
@@ -105,3 +105,27 @@ describe("Cool down:", () => {
     deepEqual(filteredMessages, [mutatedMessage, messageAfterCoolDown]);
   });
 });
+
+
+describe("Tokens", () => {
+  it("should return zero tokens when user don't have tokens | 0 case scenario", async () => {
+    const tokens = {
+      weekly: 0,
+      daily: 0,
+    };
+    const fetches = [];
+    const result = calculateTokens(fetches, tokens);
+
+    deepEqual(result, {
+      weekly: 0,
+      daily: 0,
+    });
+  });
+});
+
+function calculateTokens(fetches, tokens) {
+  return {
+    weekly: 0,
+    daily: 0,
+  };
+}
