760034c3ef9d79ac50505053a39faa3ab6dfdce3
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-14 17:09:26 +0100

    seeding messages with faker users and messages
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -1,18 +1,18 @@
 import { faker } from "@faker-js/faker";
-export function seedMessages(numberOfMessages: number) {
-  Array.from({ length: numberOfMessages })
-    .map(() => {
-      const content = `${faker.word.noun} ${faker.word.verb} ${faker.word.adverb}`;
+export function seedMessages(amountOfUsers: number) {
+  const usersArray = Array.from({ length: amountOfUsers }).map(() => {
+    const username = faker.person.firstName();
+    const amountOfMessages = Math.ceil(Math.random() * 4);
+    const messages = Array.from({ length: amountOfMessages }).map(() => {
+      const content = `${faker.word.noun()} ${faker.word.verb()} ${faker.word.adverb()}`;
       return {
-        username: faker.person.firstName(),
         content,
-        timestamp:
-          BigInt(Date.now()) - BigInt(Math.floor(Math.random() * 500000)),
+        timestamp: faker.date.anytime().getTime(),
       };
-    })
-    .forEach((message) => {
-      console.log(message);
     });
+    return { username, messages };
+  });
+  console.log(usersArray);
 }
 
-seedMessages(15);
+seedMessages(5);
