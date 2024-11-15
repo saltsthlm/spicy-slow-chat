6a697f9845faa09622084c596ebffbe805f823a9
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-15 08:53:59 +0100

    Add logic for test and get daysSinceMonday instead

--- src/features/chat/tests/helper.test.ts
+++ src/features/chat/tests/helper.test.ts
@@ -1,14 +1,14 @@
 import { describe, it } from "node:test";
-import { deepEqual } from "node:assert/strict";
-import { getMondayOfCurrentWeek } from "../helper";
+import { equal } from "node:assert/strict";
+import { numberOfdaysSinceMonday } from "../helper";
 
 describe("Date helper:", () => {
   it("should return correct Monday timestamp given a date", () => {
-    const date = new Date("Novemver 17, 2024 23:13:00");
-    const monday = new Date("Novemver 11, 2024 00:00:00");
+    const sunday = new Date("Novemver 17, 2024 23:13:00");
+    const daysSinceMonday = 6;
 
-    const result = getMondayOfCurrentWeek(date);
+    const result = numberOfdaysSinceMonday(sunday);
 
-    deepEqual(result, monday);
+    equal(result, daysSinceMonday);
   });
 });
