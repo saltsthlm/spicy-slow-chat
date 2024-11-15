00414f6c810fa2195105f809cfd215e58da6422f
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-15 08:51:03 +0100

    Add another describe level

--- src/features/chat/tests/tokens.test.ts
+++ src/features/chat/tests/tokens.test.ts
@@ -3,32 +3,34 @@ import { describe, it } from "node:test";
 import { calculateRemainingTokens } from "../logic/tokens";
 
 describe("Token Calculation:", () => {
-  it("should return initial tokens when there are 0 fetches | 0 case scenario", async () => {
-    const tokens = { weekly: 0, daily: 0 };
-    const fetches = 0;
-    const result = calculateRemainingTokens(fetches, tokens);
+  describe("by day:", () => {
+    it("should return initial tokens when there are 0 fetches | 0 case scenario", async () => {
+      const tokens = { weekly: 0, daily: 0 };
+      const fetches = 0;
+      const result = calculateRemainingTokens(fetches, tokens);
 
-    deepEqual(result, { weekly: 0, daily: 0 });
-  });
+      deepEqual(result, { weekly: 0, daily: 0 });
+    });
 
-  it("should return right amount of tokens | 1 case scenario", () => {
-    const tokens = { weekly: 2, daily: 1 };
-    const fetches = 1;
-    const result = calculateRemainingTokens(fetches, tokens);
-    deepEqual(result, { weekly: 2, daily: 0 });
-  });
+    it("should return right amount of tokens | 1 case scenario", () => {
+      const tokens = { weekly: 2, daily: 1 };
+      const fetches = 1;
+      const result = calculateRemainingTokens(fetches, tokens);
+      deepEqual(result, { weekly: 2, daily: 0 });
+    });
 
-  it("should return right amount of tokens | many case scenario", () => {
-    const tokens = { weekly: 2, daily: 1 };
-    const fetches = 2;
-    const result = calculateRemainingTokens(fetches, tokens);
-    deepEqual(result, { weekly: 1, daily: 0 });
-  });
+    it("should return right amount of tokens | many case scenario", () => {
+      const tokens = { weekly: 2, daily: 1 };
+      const fetches = 2;
+      const result = calculateRemainingTokens(fetches, tokens);
+      deepEqual(result, { weekly: 1, daily: 0 });
+    });
 
-  it("should return right amount of tokens | many case scenario", () => {
-    const tokens = { weekly: 2, daily: 1 };
-    const fetches = 3;
-    const result = calculateRemainingTokens(fetches, tokens);
-    deepEqual(result, { weekly: 0, daily: 0 });
+    it("should return right amount of tokens | many case scenario", () => {
+      const tokens = { weekly: 2, daily: 1 };
+      const fetches = 3;
+      const result = calculateRemainingTokens(fetches, tokens);
+      deepEqual(result, { weekly: 0, daily: 0 });
+    });
   });
 });
