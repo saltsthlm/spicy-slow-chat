2aca4bc2c83cca1c4d36bef9bba0ce32120cde19
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-15 08:53:59 +0100

    Add logic for test and get daysSinceMonday instead

--- src/features/chat/helper.ts
+++ src/features/chat/helper.ts
@@ -1,7 +1,5 @@
-export function getMondayOfCurrentWeek(currentDate: Date) {
+export function numberOfdaysSinceMonday(currentDate: Date) {
   const day = currentDate.getDay();
-  const diff = currentDate.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
-  const currentMonday = new Date(currentDate.setDate(diff));
-  currentMonday.setHours(0,0,0,0)
-  return currentMonday;
+  const diff = day - (day == 0 ? -6 : 1);
+  return diff;
 }
