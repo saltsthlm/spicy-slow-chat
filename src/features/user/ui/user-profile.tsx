f3603b0e324739d5bb79898978ef23963a92bc5f
Odin Thif <mjd.thif@icloud.com>
2024-11-15 12:20:05 +0100

    update user profile update
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/user/ui/user-profile.tsx
+++ src/features/user/ui/user-profile.tsx
@@ -2,8 +2,25 @@ import { userFeature } from "../instance";
 
 export async function UserProfile() {
   const user = await userFeature.userService.getUser("John Wick");
-  console.log(user[0]);
-  const userName = user[0].username;
 
-  return <div>{userName && userName}</div>;
+  const username = user[0].username;
+
+  return (
+    <>
+      <div
+        className={`p-4 w-full m-2 flex rounded-sm ${
+          username ? "bg-gray-300" : "bg-gray-300"
+        }`}
+      >
+        {username ? (
+          <h1>Welcome, {username}!</h1>
+        ) : (
+          <div>
+            <input placeholder="Enter your name" />
+            <button className="bg-white p-3">Login</button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
