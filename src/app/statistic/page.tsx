3502759221c78185174a0b0f3f2a086c637448de
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-16 10:05:22 +0100

    Call service inside Statistics component to get message and fetch counts
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/app/statistic/page.tsx
+++ src/app/statistic/page.tsx
@@ -1,5 +1,7 @@
 import { Statistics } from "@/features/";
 
 export default async function Home() {
+  const amountOfMessages = await statisticsFeature.service.getMessageCount();
+  const amountOfFetches = await statisticsFeature.service.getFetchCount();
   return <Statistics />;
 }
