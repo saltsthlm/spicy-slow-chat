1ca90962fa652e06b1d54735bb6b96ffa4f861c6
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-16 10:32:33 +0100

    added statistics feature
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

---
+++ src/features/statistic/feature.ts
@@ -0,0 +1,10 @@
+import { createRepository } from "./repository";
+import { createService } from "./service";
+export function createFeature() {
+  const repository = createRepository();
+
+  const service = createService(repository);
+  return {
+    service,
+  };
+}
