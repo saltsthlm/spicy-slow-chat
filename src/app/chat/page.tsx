910011e7be6ad8c15216950b36285b6e35ffdb2b
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-17 12:32:50 +0100

    added chatpage and moved all home content to chat content
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,33 @@
+import {
+  chatFeature,
+  MessageInput,
+  ChatFeed,
+  Card,
+  CardTitle,
+  CardHeader,
+} from "@/features";
+import { UserProfile } from "@/features/user/ui/user-profile";
+
+export default async function Chat() {
+  const messages = await chatFeature.service.getAllMessages();
+  const numberOfTokens = await chatFeature.service.getUserTokens("John Wick");
+
+  return (
+    <div className="h-screen max-w-screen-sm m-auto flex flex-col items-center py-4">
+      <UserProfile />
+      <Card className="flex flex-col items-center m-auto px-6 w-full max-h-full overflow-y-auto flex-grow">
+        <CardHeader>You have {numberOfTokens} fetch tokens.</CardHeader>
+        <CardTitle className="w-full">
+          <h2 className="scroll-m-20 border-b text-md font-normal tracking-normal first:mt-0 pt-4 pb-3 w-full">
+            Spicy Members
+          </h2>
+        </CardTitle>
+        <div className="flex-grow w-full overflow-y-auto">
+          <ChatFeed messages={messages} />
+        </div>
+        <div className="w-full"></div>
+        <MessageInput />
+      </Card>
+    </div>
+  );
+}
