961abd023bcece5806848f11bcc9962db1c52745
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-17 11:41:20 +0100

    Set up Nav Bar and LoginPage
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/app/ui-components/navbar/navbar.tsx
+++ src/app/ui-components/navbar/navbar.tsx
@@ -1,8 +1,19 @@
-const NavItems = [{name: "Login", href: "/login"},{name: "Statistics", href: "/statistics"}]
+import Link from "next/link";
+
+const NavItems = [
+  { name: "Login", href: "/login-page" },
+  { name: "Statistics", href: "/statistic" },
+];
 export function Navbar() {
   return (
-    <ul>
-
+    <ul className="bg-gray-300 flex space-x-4">
+      {NavItems.map((navItem, index) => {
+        return (
+          <li key={index}>
+            <Link href={navItem.href}>{navItem.name}</Link>
+          </li>
+        );
+      })}
     </ul>
-  )
+  );
 }
